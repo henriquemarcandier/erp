a8adea9d546c5b884cae30a69d8b3bc2232b0958
Henrique Marcandier <henrique.marcandier@gmail.com>
2025-07-19 13:07:42 -0300

    second commit

--- views/layout/footer.php
+++ views/layout/footer.php
@@ -1,2 +1,8 @@
+<footer class="bg-light text-center text-muted py-3 mt-5">
+  <div class="container">
+    <small>&copy <?=date('Y')?> Desenvolvido por Henrique Marcandier</small>
+  </div>
+</footer>
+
 </body>
 </html>
