cbefe8bff2914bc70757958a034d77f7aa82e163
Henrique Marcandier <henrique.marcandier@gmail.com>
2025-07-21 01:17:39 -0300

    first commit

--- models/Cupom.php
+++ models/Cupom.php
@@ -4,12 +4,18 @@ require_once __DIR__ . '/../config/database.php';
 class Cupom {
     public static function buscar($codigo) {
         global $conn;
+        $_REQUEST['codigo'] = ($_REQUEST['codigo'] ?? '');
+        $_REQUEST['validade'] = ($_REQUEST['validade'] ?? '');
+        $_REQUEST['cupom'] = ($_REQUEST['cupom'] ?? '');
         $codigo = strtoupper(trim($_REQUEST['codigo']));
         $codigo = $conn->real_escape_string($codigo);
+        $cupom = strtoupper(trim($_REQUEST['cupom']));
+        $cupom = $conn->real_escape_string($cupom);
+        $codigo = ($codigo ? $codigo : $cupom);
         $validade = strtoupper(trim($_REQUEST['validade']));
         $validade = $conn->real_escape_string($validade);
-        echo $codigo."--".$validade."--";
-        $res = $conn->query("SELECT * FROM cupons WHERE codigo = '$codigo' AND validade >= CURDATE()");
+        $sql = "SELECT * FROM cupons WHERE codigo = '$codigo' AND validade >= CURDATE()";
+        $res = $conn->query($sql);
         return $res->fetch_assoc();
     }
 }
